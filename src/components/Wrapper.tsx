72afec909c957a1d9d8d8181b451d24ad6f5f9bd
misiekhardcore <misiekhardcore@gmail.com>
2021-03-05 21:56:36 +0100

    modify Wrapper params

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -1,8 +1,22 @@
 import { Box } from "@chakra-ui/layout";
 import React from "react";
 
-interface WrapperProps {}
+interface WrapperProps {
+  variant?: "small" | "regular";
+}
 
-export const Wrapper: React.FC<WrapperProps> = ({ children }) => {
-  return <Box>{children}</Box>;
+export const Wrapper: React.FC<WrapperProps> = ({
+  children,
+  variant = "regular",
+}) => {
+  return (
+    <Box
+      mx="auto"
+      mt={8}
+      maxW={variant === "regular" ? "800px" : "400px"}
+      w="100%"
+    >
+      {children}
+    </Box>
+  );
 };
