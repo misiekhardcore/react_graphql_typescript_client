8b553e02b8d2009323291380c69831d1c6c83101
misiekhardcore <misiekhardcore@gmail.com>
2021-03-07 00:27:44 +0100

    create login page

---
+++ src/pages/login.tsx
@@ -0,0 +1,62 @@
+import { Button } from "@chakra-ui/react";
+import { Form, Formik } from "formik";
+import React from "react";
+import { InputField } from "../components/InputField";
+import { Wrapper } from "../components/Wrapper";
+import { useLoginMutation } from "../generated/graphql";
+import { toErrorMap } from "../utils/toErrormap";
+import { useRouter } from "next/router";
+import { withUrqlClient } from "next-urql";
+import { createUrqlClient } from "../utils/createUrqlClient";
+
+interface loginProps {}
+
+export const Login: React.FC<loginProps> = ({}) => {
+  const router = useRouter();
+  const [, login] = useLoginMutation();
+
+  return (
+    <Wrapper variant="small">
+      <h1>Login</h1>
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={async (values, { setErrors }) => {
+          const response = await login({
+            options: values,
+          });
+          if (response.data?.login?.errors) {
+            setErrors(toErrorMap(response.data.login.errors));
+          } else if (response.data?.login?.user) {
+            router.push("/");
+          }
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              placeholder="Enter your username"
+              label="Username"
+              name="username"
+            />
+            <InputField
+              placeholder="Enter your password"
+              label="Password"
+              name="password"
+              type="password"
+            />
+            <Button
+              mt={4}
+              isLoading={isSubmitting}
+              type="submit"
+              colorScheme="teal"
+            >
+              login
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
+
+export default withUrqlClient(createUrqlClient)(Login);
