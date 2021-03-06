8fcf0683995f1967a845ea9c46be7b6f625e4359
misiekhardcore <misiekhardcore@gmail.com>
2021-03-07 00:27:21 +0100

    add navbar and use urql client

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,21 @@
-const Index = () => <div>hello world</div>;
+import { withUrqlClient } from "next-urql";
+import { Navbar } from "../components/Navbar";
+import { createUrqlClient } from "../utils/createUrqlClient";
+import { usePostsQuery } from "../generated/graphql";
 
-export default Index;
+const Index = () => {
+  const [{ data }] = usePostsQuery();
+  return (
+    <>
+      <Navbar />
+      <div>hello world</div>
+      {data ? (
+        data.posts.map((post) => <div key={post.id}>{post.title}</div>)
+      ) : (
+        <p>loading...</p>
+      )}
+    </>
+  );
+};
+
+export default withUrqlClient(createUrqlClient, { ssr: true })(Index);
