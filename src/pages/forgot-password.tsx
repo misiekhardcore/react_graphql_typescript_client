3c6ced1e23bd061cbb07168acc25a21d95d1ec83
misiekhardcore <misiekhardcore@gmail.com>
2021-03-08 14:31:14 +0100

    rename file

--- src/pages/forgot-password.tsx
+++ src/pages/forgot-password.tsx
@@ -1,6 +1,5 @@
 import { Box, Button } from "@chakra-ui/react";
 import { Formik, Form } from "formik";
-import router from "next/router";
 import React, { useState } from "react";
 import { InputField } from "../components/InputField";
 import { Wrapper } from "../components/Wrapper";
