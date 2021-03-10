fe695ce7113c9feb51176918d5f75a8bddf70394
misiekhardcore <misiekhardcore@gmail.com>
2021-03-10 14:20:29 +0100

    add textarea variant

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -4,6 +4,7 @@ import {
   FormLabel,
   Input,
   FormErrorMessage,
+  Textarea,
 } from "@chakra-ui/react";
 import { useField } from "formik";
 
@@ -11,19 +12,27 @@ type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
   name: string;
   label: string;
   mt?: any;
+  textarea?: boolean;
 };
 
 export const InputField: React.FC<InputFieldProps> = ({
   label,
   size: _,
   mt = 0,
+  textarea = false,
   ...props
 }) => {
+  let InputOrTextarea: any;
+  if (textarea) {
+    InputOrTextarea = Textarea;
+  } else {
+    InputOrTextarea = Input;
+  }
   const [field, { error }] = useField(props);
   return (
     <FormControl mt={mt} isInvalid={!!error}>
       <FormLabel htmlFor={field.name}>{label}</FormLabel>
-      <Input {...field} {...props} id={field.name} />
+      <InputOrTextarea {...field} {...props} id={field.name} />
       {error ? <FormErrorMessage>{error}</FormErrorMessage> : null}
     </FormControl>
   );
