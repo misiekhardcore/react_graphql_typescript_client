38788ed991e0e53e8690e9065f3f019ff794b025
misiekhardcore <misiekhardcore@gmail.com>
2021-03-11 13:15:56 +0100

    use Layout and add temp lin to createPost

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,25 @@
 import { withUrqlClient } from "next-urql";
-import { Navbar } from "../components/Navbar";
 import { createUrqlClient } from "../utils/createUrqlClient";
 import { usePostsQuery } from "../generated/graphql";
+import { Layout } from "../components/Layout";
+import NextLink from "next/link";
+import { Link } from "@chakra-ui/layout";
 
 const Index = () => {
   const [{ data }] = usePostsQuery();
   return (
-    <>
-      <Navbar />
+    <Layout>
       <div>hello world</div>
       {data ? (
         data.posts.map((post) => <div key={post.id}>{post.title}</div>)
       ) : (
         <p>loading...</p>
       )}
-    </>
+      <br />
+      <NextLink href="/create-post">
+        <Link>Create Post</Link>
+      </NextLink>
+    </Layout>
   );
 };
 
